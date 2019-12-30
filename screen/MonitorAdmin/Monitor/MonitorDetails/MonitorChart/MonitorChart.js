80046cb95df8616c57dfe8447efcab740296ce38
Cristyan Sepúlveda V <cristyan.sv@gmail.com>
2019-12-30 09:04:22 -0500

    #9 send email notifications (#11)
    * change
    
    * #9 send emails notifications

--- screen/MonitorAdmin/Monitor/MonitorDetails/MonitorChart/MonitorChart.js
+++ screen/MonitorAdmin/Monitor/MonitorDetails/MonitorChart/MonitorChart.js
@@ -1,85 +1,100 @@
 define("Monitor", {
-    data: function () {
-        return {
-            alertCount: 0,
-            alerts: [],
-            displayFromDate: '',
-            displayThruDate: '',
-            monitorId: '',
-            monitor: null
-        };
+  data: function() {
+    return {
+      alertCount: 0,
+      alerts: [],
+      displayFromDate: "",
+      displayThruDate: "",
+      monitorId: "",
+      monitor: null,
+      selectedAlert: null
+    };
+  },
+  mounted: function() {
+    const librariesPromises = [];
+    librariesPromises.push(this.loadAxiosLibrary());
+
+    const fromDate = new Date(Date.now() - 1000 * 60 * 60 * 24 * 5);
+    const thruDate = new Date(Date.now() + 1000 * 60 * 100);
+
+    Promise.all(librariesPromises).then(() => {
+      this.monitorId = this.$root.currentParameters.monitorId;
+      this.getMonitorInfo();
+      this.getAlerts(fromDate, thruDate);
+    });
+  },
+  methods: {
+    selectAlert(alert) {
+      this.selectedAlert = alert;
     },
-    mounted: function () {
-        const librariesPromises = [];
-        librariesPromises.push(this.loadAxiosLibrary());
+    getMonitorInfo() {
+      const url = "/rest/s1/tailorsoft/monitors/" + this.monitorId;
+      let vm = this;
 
-        const fromDate = new Date(Date.now() - 1000 * 60 * 60 * 24 * 5);
-        const thruDate = new Date(Date.now() + 1000 * 60 * 100);
-
-        Promise.all(librariesPromises).then(() => {
-            this.monitorId = this.$root.currentParameters.monitorId;
-            this.getMonitorInfo();
-            this.getAlerts(fromDate, thruDate);
+      axios
+        .get(url)
+        .then(res => {
+          vm.monitor = res.data;
+          console.log(vm.monitor);
+        })
+        .catch(err => {
+          console.log(err);
         });
     },
-    methods: {
-        getMonitorInfo(){
-            const url = '/rest/s1/tailorsoft/monitors/' + this.monitorId;
-            let vm = this;
+    getAlerts(_fromDate, _thruDate) {
+      const fromDate = moment(_fromDate).startOf("day");
+      const thruDate = moment(_thruDate).endOf("day");
 
-            axios.get(url).then(res=>{
-                vm.monitor = res.data;
-                console.log(vm.monitor)
-            }).catch(err=>{
-                console.log(err)
-            })
-        },
-        getAlerts(_fromDate, _thruDate) {
-            const fromDate = moment(_fromDate).startOf('day');
-            const thruDate = moment(_thruDate).endOf('day');
+      this.displayFromDate = fromDate.format("YYYY-MM-DD");
+      this.displayThruDate = thruDate.format("YYYY-MM-DD");
 
-            this.displayFromDate = fromDate.format('YYYY-MM-DD');
-            this.displayThruDate = thruDate.format('YYYY-MM-DD');
+      const url = `/rest/s1/tailorsoft/monitors?fromDate=${fromDate.toISOString()}&thruDate=${thruDate.toISOString()}&monitorId=${
+        this.monitorId
+      }`;
 
-            const url = `/rest/s1/tailorsoft/monitors?fromDate=${fromDate.toISOString()}&thruDate=${thruDate.toISOString()}&monitorId=${this.monitorId}`;
+      axios
+        .get(url)
+        .then(res => {
+          this.data = res.data.value[0];
+          this.alerts = this.data.alerts.map(alert => {
+            return {
+              id: alert.id,
+              fromDate: moment(alert.fromDate).format("DD MMM, h:mm a"),
+              thrumDate: moment(alert.thruDate).format("DD MMM, h:mm a"),
+              status: alert.statusId
+            };
+          }).reverse();
 
-            axios
-                .get(url)
-                .then(res => {
-                    this.data = res.data.value[0];
-                    this.alerts = this.data.alerts.map(alert => {
-                        return {
+          renderVega(this.data, "chartjsContainer");
+          this.alertCount = this.alerts.length;
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    },
+    loadAxiosLibrary() {
+      return new Promise(function(resolve, reject) {
+        const importAxios = document.createElement("script");
+        const scriptType = "text/javascript";
+        importAxios.type = scriptType;
+        importAxios.src =
+          "https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js";
+        importAxios.async = false;
+        importAxios.onload = function() {
+          resolve();
+        };
+        document.head.appendChild(importAxios);
+      });
+    },
+    setDates({ type, target }) {
+      const fromDate = moment(this.displayFromDate, "YYYY-MM-DD")
+        .startOf("day")
+        .toDate();
+      const thruDate = moment(this.displayThruDate, "YYYY-MM-DD")
+        .endOf("day")
+        .toDate();
 
-                            fromDate: moment(alert.fromDate).format("DD MMM, h:mm a")
-                        };
-                    });
-
-                    renderVega(this.data, "chartjsContainer");
-                    this.alertCount = this.alerts.length;
-                })
-                .catch(function (err) {
-                    console.log(err);
-                });
-        },
-        loadAxiosLibrary() {
-            return new Promise(function (resolve, reject) {
-                const importAxios = document.createElement("script");
-                const scriptType = "text/javascript";
-                importAxios.type = scriptType;
-                importAxios.src =
-                    "https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js";
-                importAxios.async = false;
-                importAxios.onload = function () {
-                    resolve();
-                };
-                document.head.appendChild(importAxios);
-            });
-        },
-        setDates({type, target}) {
-            const fromDate = moment(this.displayFromDate, 'YYYY-MM-DD').startOf('day').toDate();
-            const thruDate = moment(this.displayThruDate, 'YYYY-MM-DD').endOf('day').toDate();
-
-            this.getAlerts(moment(fromDate), moment(thruDate))
-        }
+      this.getAlerts(moment(fromDate), moment(thruDate));
     }
+  }
 });
